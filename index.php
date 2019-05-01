d599cf2871b5d3bda052b588ef7e32f01e46c416
Roberto Torres <roberto.torres@mavs.uta.edu>
2019-05-01 08:28:21 -0500

    Revert "Update index.php"
    This reverts commit 38b4682a76bece75a17cd5b3e27e4a2d563c1598.

--- index.php
+++ index.php
@@ -10,14 +10,24 @@
     <?php
       $server= "localhost";
       $username = "root";
+      $password = ""; //enter your mysql password here
+      $database = "SHELTER";
 
+      $connection = new mysqli($server, $username, $password, $database);
       if($connection->connect_error){
         die("Connection failed: " ); 
+      }
     //  if($connection->query("INSERT INTO user (User_ID) VALUES ('10000000')")=== TRUE)
     //     echo "New User record inserted.";
     //  else
     //     echo "Error inserting record.";
-    $connection->close();
+     $result = $connection->query("SELECT User_ID from user");
+     if($result->num_rows > 0){
+        while($row = $result->fetch_assoc())
+          echo $row[User_ID] . "<br>";
+      }
+     else echo "Query error";
+      $connection->close();
     ?>
   </body>
 </html>
