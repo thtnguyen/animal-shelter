53b8f51fa6e494658a421830db23391c590f1ade
Teresa Nguyen <teresa.nguyen@mavs.uta.edu>
2019-05-01 15:29:25 -0500

    fixed index.php

--- index.php
+++ index.php
@@ -6,28 +6,27 @@
     <title> Animal Shelter </title>
   </head>
   <body>
-    <h1 class="header"> Hello Animal Shelter </h1>
-    <?php
-      $server= "localhost";
-      $username = "root";
-      $password = ""; //enter your mysql password here
-      $database = "SHELTER";
+    <h1 class="header"> Animal Shelter </h1>
 
-      $connection = new mysqli($server, $username, $password, $database);
-      if($connection->connect_error){
-        die("Connection failed: " ); 
-      }
+    <a href="registerForm.php">Register User</a>
+		<br/>
+
+    <a href="animalForm.php">New Animal</a>
+		<br/>
+		
+    
+    <?php
     //  if($connection->query("INSERT INTO user (User_ID) VALUES ('10000000')")=== TRUE)
     //     echo "New User record inserted.";
     //  else
     //     echo "Error inserting record.";
-     $result = $connection->query("SELECT User_ID from user");
-     if($result->num_rows > 0){
-        while($row = $result->fetch_assoc())
-          echo $row[User_ID] . "<br>";
-      }
-     else echo "Query error";
-      $connection->close();
+    //  $result = $connection->query("SELECT User_ID from user");
+    //  if($result->num_rows > 0){
+    //     while($row = $result->fetch_assoc())
+    //       echo $row[User_ID] . "<br>";
+    //   }
+    //  else echo "Query error";
+    //   $connection->close();
     ?>
   </body>
 </html>
